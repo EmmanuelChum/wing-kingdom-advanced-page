544c8fb62831488c998b765da75f58afb6a41e65
Emmanuel Chum <emmanuelchum7@gmail.com>
2022-10-05 05:22:09 -0400

    tenth commit

--- src/app/shared/PARTNERS.js
+++ src/app/shared/PARTNERS.js
@@ -14,7 +14,7 @@ export const PARTNERS = [
     },
     {
         id: 1,
-        name: 'Level Dos',
+        name: 'Level DOS',
         image: gitLogo,
         featured: false,
         description:
@@ -24,7 +24,7 @@ export const PARTNERS = [
         id: 2,
         name: 'Level III',
         image: mongoLogo,
-        featured: false,
+        featured: true,
         description:
             "I'm not crying, are you crying?  We understand if you want to throw in that towel now, we won't judge you.  But your family and friends might, and your crush who's secretly sitting in the background.  Hehe no pressure."
     },
@@ -32,7 +32,7 @@ export const PARTNERS = [
         id: 3,
         name: 'Level FIVE Alarm!!!',
         image: nodeLogo,
-        featured: true,
+        featured: false,
         description:
             "Yeah... we went ahead and just skipped to 5, anything lower wouldn't do this flavor justice.  We're not calling you insane for doing this, but you're pretty much insane.  It's not that we don't believe in you but, just to be safe ...SIGN THAT WAIVER!!!"
     }
